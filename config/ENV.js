fc91d7962d543c59044036344ab68d70d5e92804
ivin-austan <a.ivinaustan@gmail.com>
2025-10-04 12:05:13 +0400

    chore: updated the configs

--- config/ENV.js
+++ config/ENV.js
@@ -1,5 +1,5 @@
 export const REACT_SERVER_URL =
-  import.meta.env.VITE_ENVIRONMENT === "production"
+  import.meta.env.VITE_ENVIRONMENT === "development"
     ? import.meta.env.VITE_REACT_SERVER_URL
     : import.meta.env.VITE_REACT_PROD_SERVER_URL;
 export const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
