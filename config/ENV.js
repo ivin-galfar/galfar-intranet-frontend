d6d29ef2f48f31cc3f9cebeca0b11e69a04c9f6d
ivin-austan <a.ivinaustan@gmail.com>
2025-10-04 10:17:31 +0400

    test: testing by preventing undefined logic

--- config/ENV.js
+++ config/ENV.js
@@ -1,5 +1,6 @@
 export const REACT_SERVER_URL =
   import.meta.env.VITE_ENVIRONMENT === "development"
     ? import.meta.env.VITE_REACT_SERVER_URL
-    : import.meta.env.VITE_REACT_PROD_SERVER_URL;
+    : import.meta.env.VITE_REACT_PROD_SERVER_URL ||
+      "aznuaegecbackend.azurewebsites.net";
 export const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
